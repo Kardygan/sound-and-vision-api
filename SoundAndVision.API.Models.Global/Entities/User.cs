f049e8bbddbc7a705807ab667c1791a8c69b9fb5
tommy <k.sn4ily@gmail.com>
2021-06-17 17:01:11 +0200

    Add User global entity

--- SoundAndVision.API.Models.Global/Entities/User.cs
+++ SoundAndVision.API.Models.Global/Entities/User.cs
@@ -6,6 +6,16 @@ namespace SoundAndVision.API.Models.Global.Entities
 {
     public class User
     {
-
-    }
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Password { get; set; }
+        public string Picture { get; set; }
+        public string Location { get; set; }
+        public string Bio { get; set; }
+        public DateTime RegistrationDate { get; set; }
+        public bool IsActive { get; set; }
+        public int RoleId { get; set; }
+	}
 }
